1f1bcd7f3a64342449b509b50964d8c6377718b9
FelipeAT <pipeaviles1608@gmail.com>
2025-05-07 09:52:37 -0500

    Eliminacion de User Check

--- src/app/General/generic-table/generic-table.component.ts
+++ src/app/General/generic-table/generic-table.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-generic-table',
-  imports: [],
+  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule],
   templateUrl: './generic-table.component.html',
   styleUrl: './generic-table.component.css'
 })
-export class GenericTableComponent {
+export class GenericTableComponent implements OnChanges {
+  @Input() data: any[] = [];
+  @Input() columns: string[] = [];
+
+  @Output() onDelete = new EventEmitter<any>();
+  @Output() onEdit = new EventEmitter<any>();
+
+  dataSource = new MatTableDataSource<any>();
+  displayedColumns: string[] = [];
+
+  ngOnChanges(): void {
+    this.dataSource.data = this.data || [];
+    this.displayedColumns = [...this.columns, 'actions'];
+  }
+
+  getValue(item: any, path: string): any {
+    return path.split('.').reduce((acc, key) => acc?.[key], item);
+  }
+
+  deleteItem(element: any) {
+    this.onDelete.emit(element); 
+  }
+  
+  editItem(element: any) {
+    this.onEdit.emit(element); 
+  }
+
 
 }
--- src/app/Views/indice-user/indice-user.component.ts
+++ src/app/Views/indice-user/indice-user.component.ts
@@ -1,29 +1,72 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { User } from '../../Models/user.model';
 import { UserService } from '../../Services/user.service';
+import { Router, RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { GenericTableComponent } from "../../General/generic-table/generic-table.component";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-indice-user',
-  imports: [],
+  imports: [MatCardModule, GenericTableComponent, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './indice-user.component.html',
   styleUrl: './indice-user.component.css'
 })
 export class IndiceUserComponent implements OnInit {
   
   userService = inject(UserService)
+  router = inject(Router)
 
   dataUsers: User[] = []
+  columnas: string[] = ['N°', 'name', 'username','email', 'phone', 'website'];
   
   ngOnInit(): void {
     this.userService.getAll().subscribe({
       next: (data) => {
         this.dataUsers = data;
-        console.log('Usuarios recibidos:', this.dataUsers);
+        console.log('Data:', this.dataUsers);
       },
       error: (err) => {
         console.error('Error al obtener usuarios:', err);
       }
     });
+  }
+
+  cargarUsuarios(): void {
+    this.userService.getAll().subscribe((res) => {
+      this.dataUsers = res;
+    });
+  }
+
+  eliminarUsuario(user: User): void {
+    Swal.fire({
+      title: '¿Deses Eliminar el User?',
+      text: `User: ${user.username}`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, seguro',
+      cancelButtonText: 'Cancelar',
+      confirmButtonColor: '#d33',
+    }).then(result => {
+      if (result.isConfirmed) {
+        this.userService.delete(user.id).subscribe({
+          next: () => {
+            Swal.fire('Eliminado Exitosamente', '', 'success');
+            this.cargarUsuarios();
+          },
+          error(err) {
+            console.log(err)
+          },
+        });
+      }
+    });
+  }
+
+  editarUsuario(id: number): void {
+    this.router.navigate(['/users/editar', id]);
   }
 
 }
